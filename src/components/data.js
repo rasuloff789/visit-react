f10010247f227faa42170cfccb47099cc48eae26
rasuloff789 <rasuloff789@gmail.com>
2020-12-12 22:13:47 +0500

    data export

---
+++ src/components/data.js
@@ -0,0 +1,42 @@
+export let people = [
+  {
+    id : 0 , name : "Rasul Ismoilov" 
+  },
+  {
+    id : 1 , name : "Usmon Masudjonov" 
+  },
+  {
+    id : 2 , name : "Jahongir Adizov" 
+  },
+  {
+    id : 3 , name : "John Terry" 
+  }
+  
+] ;
+
+export let dataUsers = [
+  {
+    id : 0 , userId : 0 , visitDate : "2020-12-12" , visit : true , score : 2
+  },
+  {
+    id : 1 , userId : 2 , visitDate : "2020-12-12" , visit : false , score : 0
+  },
+  {
+    id : 2 , userId : 1 , visitDate : "2020-12-12" , visit : false , score : 0
+  },
+  {
+    id : 3 , userId : 3 , visitDate : "2020-12-12" , visit : true , score : 1
+  },
+  {
+    id : 4 , userId : 0 , visitDate : "2020-10-12" , visit : false , score : 0
+  },
+  {
+    id : 5 , userId : 2 , visitDate : "2020-10-12" , visit : true , score : 5
+  },
+  {
+    id : 6 , userId : 1 , visitDate : "2020-10-12" , visit : true , score : 5
+  },
+  {
+    id : 7 , userId : 3 , visitDate : "2020-10-12" , visit : true , score : 1
+  }
+] ;
